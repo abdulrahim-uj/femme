59c1f12934bfb9835661563684031bf7df989e88
Abdul Rahim Kattirithodi <abdulrahim.uj@gmail.com>
2022-10-09 10:05:49 -0700

    basic setup and urls basics

---
+++ src/femme/web/views.py
@@ -0,0 +1,8 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
+
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello Django");
