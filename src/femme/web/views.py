70c26b4ccaf06130e695d1d030b5762f53c9a73d
Abdul Rahim Kattirithodi <abdulrahim.uj@gmail.com>
2022-10-09 10:31:04 -0700

    url redirect

--- src/femme/web/views.py
+++ src/femme/web/views.py
@@ -1,8 +1,12 @@
 from django.http.response import HttpResponse, HttpResponseRedirect
-
+from django.urls import reverse
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Django");
+    return HttpResponseRedirect(reverse('web:test_redirect'));
+
+
+def test(request):
+    return HttpResponse("Hello Django Again from test redirect");
