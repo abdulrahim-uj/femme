bc56e6b724f709132063c070d2a001bbe5a6d48d
Abdul Rahim Kattirithodi <abdulrahim.uj@gmail.com>
2022-10-10 00:14:31 -0700

    first model registration created

--- src/femme/web/models.py
+++ src/femme/web/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class RegistrationClass(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.EmailField()
+    phone = models.CharField(max_length=128)
+    education = models.CharField(max_length=128)
+    dob = models.DateField()
+    message = models.TextField()
+
+    class Meta:
+        db_table = 'web_registration'
+        verbose_name = _('registration')
+        verbose_name_plural = _('registrations')
+
+    def __unicode__(self):
+        return self.name
