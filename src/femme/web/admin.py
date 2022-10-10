ff3f066fa020142230878cb3b4aaf31ab846d4ea
Abdul Rahim Kattirithodi <abdulrahim.uj@gmail.com>
2022-10-10 00:45:50 -0700

    admin registration view added with superuser creation

--- src/femme/web/admin.py
+++ src/femme/web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from web.models import RegistrationClass
+
 
 # Register your models here.
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'dob', 'education', 'message')
+
+
+admin.site.register(RegistrationClass, RegistrationAdmin)
